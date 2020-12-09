c5dc8de0d8424bc2f57f69ed412aa49fad37d517
Paul Lemieux <paul@Pauls-iMac.local>
2020-12-09 13:28:26 -0800

    Added comments to day 9

--- day9/day9.rb
+++ day9/day9.rb
@@ -2,7 +2,7 @@ input = File.readlines("input.txt").each {|line| line.strip!} #read in the file 
 window = []					#empty array to contain the 25 elements of the sliding window
 invalid = 0					#integer that will contain the invalid integer
 25.times do |i|				#build the sliding window
-	window << input[i].to_i
+	window << input[i].to_i		#this adds to the array as an integer value
 end
 input.each_with_index do |x, i|
 	if i < 25; next end 	#skip the preamble, we dont need to test that
@@ -28,16 +28,16 @@ end
 count = 0					#counter for tracking the total
 window = []						#reset the window to store the continguous elements
 input.each_with_index do |x, i|
-	j = i
-	until (count > invalid)
-		window << input[j].to_i
-		count = count + input[j].to_i
-		if count == invalid
+	j = i 					#establish a sub counter we can increment
+	until (count > invalid)		#keep going until our counter is larger than our target
+		window << input[j].to_i		#add the current subelement to our window to check against
+		count = count + input[j].to_i	#increase our sub counter by the amount of the current element
+		if count == invalid			#check to see if we hit our target exactly
 			puts "Found the weakness.  #{window.max} and #{window.min} in list. Answer is #{window.min + window.max}" 
 			exit
 		end
-		j += 1
+		j += 1		#incrememnt our window counter
 	end
-	window = []
-	count = 0
+	window = []		#reset the window for the next run
+	count = 0		#reset the counter for the next run
 end
